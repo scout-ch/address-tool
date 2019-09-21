b609a38d60dbd6fed58f515a86bc5700e98f1e39
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-09-21 23:11:00 +0200

    Implement basic mailing

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\TestMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller{
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//$this->middleware('auth');
+	}
+
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
+		return view('mail');
+	}
+
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function send(Request $request)
+	{
+		print_r($request->post());
+
+		Mail::to($request->input('email'))->send(new TestMail());
+
+		return redirect()->back();
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/mail', 'MailController@index')->name('mail');
+Route::any('/mail/send', 'MailController@send')->name('mail-send');
