3eb24f787d726737384aa51f1f5665aad04e0d4f
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-09-21 23:34:19 +0200

    Add URLs to .env

--- config/urls.php
+++ config/urls.php
@@ -2,10 +2,10 @@
 
 return [
     'groups_url' => [
-        'start' => 'https://pbs.puzzle.ch/de/groups/',
-        'end' => '.json?token=HsxUFJy_oMhivtu-at3b7AzHgqfPbBNBo6ejiz2k9ut6jzjq6g'
+        'start' => env('GROUP_URL_START', 'https://pbs.puzzle.ch/de/groups/'),
+        'end' => env('GROUP_URL_END', '.json?token=HsxUFJy_oMhivtu-at3b7AzHgqfPbBNBo6ejiz2k9ut6jzjq6g')
     ],
-    'person_url' => 'https://pbs.puzzle.ch/de/people/'
+    'person_url' => env('PERSON_URL', 'https://pbs.puzzle.ch/de/people/')
 ];
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,4 @@ Route::get('/mail', 'MailController@index')->name('mail');
 Route::any('/mail/send', 'MailController@send')->name('mail-send');
 
 Route::get('/upload', 'UploadController@index')->name('upload');
-
 Route::post('/upload_receiver', 'UploadController@receive')->name('receive');
