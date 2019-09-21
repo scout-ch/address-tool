ea62aad736abf640835866841756227002ed8ac9
Jan <jan.m.muller@icloud.com>
2019-09-21 23:22:20 +0200

    Make first oAuth authentication work.
    Next steps: get the actual data/information that we need and cleanup.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,23 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
+
+                    <div class="form-group row">
+            <div class="col-md-6 offset-md-3">
+                <a
+                    class="btn btn-hitobito form-control{{ $errors->has('hitobito') ? ' is-invalid' : '' }}"
+                    style="width: 100%"
+                    href="{{ route('login.hitobito') }}">
+                    {{ __('Via PBS MiData einloggen') }}
+                </a>
+                @if ($errors->has('hitobito'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('hitobito') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
